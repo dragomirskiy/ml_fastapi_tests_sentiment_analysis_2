826b1085e4448d8ac9f49ae6031f56cda06d9d74
dragomirskiy <91115293+dragomirskiy@users.noreply.github.com>
2023-04-04 17:09:25 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,7 +12,7 @@ def test_read_main():
 
 def test_predict_positive():
     response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
+                                json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
@@ -20,7 +20,7 @@ def test_predict_positive():
 
 def test_predict_positive_2():
     response = client.post("/predict/",
-                           json={"text": "I enjoy sport!"})
+                                json={"text": "I enjoy sport!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
@@ -28,15 +28,16 @@ def test_predict_positive_2():
 
 def test_negative_sentiment_analysis_1():
     response_post = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
-    json_data =  response_post.json()
-    assert  response_post.status_code == 200
+                                json={"text": "I hate machine learning!"})
+    json_data = response_post.json()
+    assert response_post.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
 
 
 def test_negative_sentiment_analysis_2():
     response_post = client.post("/predict/",
-                           json={"text": "I don't like to drink milk!"})
+                                json={"text": "I don't like to drink milk!"})
     json_data = response_post.json()
     assert response_post.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
