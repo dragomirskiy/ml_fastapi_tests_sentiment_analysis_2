14139fcfb0a3b12821abef26d16e9f3be4e1f00b
dragomirskiy <91115293+dragomirskiy@users.noreply.github.com>
2023-04-08 09:54:36 +0300

    Update test_main.py
    Added comments for the required libraries and comments for the test test_negative_sentiment_analysis_1()ю

--- test_main.py
+++ test_main.py
@@ -1,6 +1,11 @@
+# Подключим TestClient — клиент для тестрования API из FastAPI.
 from fastapi.testclient import TestClient
+
+# Импортируем объект app класса FastAPI из файл main.py в текущем каталоге.
+# В файле main.py содержится код приложения API для модели машинного обучения.
 from main import app
 
+# Создание клиента для тестирования, которому при создании передается объект API app
 client = TestClient(app)
 
 
@@ -25,12 +30,18 @@ def test_positive_sentiment_analysis_2():
     assert response_post.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
-
+# Проверяет тональность отрицательной фразы.
 def test_negative_sentiment_analysis_1():
+    # Клиент передает запрос POST, путь на сервере "/predict/".
+    # В теле сообщения в формате JSON передается сообщение для определения тональности.
     response_post = client.post("/predict/",
                                 json={"text": "I hate machine learning!"})
+    # Тело ответа из формата JSON в виде словаря сохраняется в переменную json_data для последующего анализа.
     json_data = response_post.json()
+    # Проверяет доступность приложения. Код статуса ответа HTTP должен быть равен 200.
     assert response_post.status_code == 200
+    # Проверяет результат распознавания. Тональность фразы положительная, поэтому
+    # результат определения тональности (ключ 'label' в словаре json_data) должен быть 'NEGATIVE'.
     assert json_data['label'] == 'NEGATIVE'
 
 
