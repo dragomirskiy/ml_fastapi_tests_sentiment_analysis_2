8decc7368d7eaf90d54d89ec0b20068b12807ae7
dragomirskiy <91115293+dragomirskiy@users.noreply.github.com>
2023-04-10 18:05:10 +0300

    Update main_for_streamlit.py

--- main_for_streamlit.py
+++ main_for_streamlit.py
@@ -19,23 +19,6 @@ def sentiment_and_score(inputs):
     return result['label'], result['score']
 
 
-# if __name__ == '__main__':
-#     try:
-#         # Определение тональности текста.
-#         st.title('ОПРЕДЕЛЕНИЕ ТОНАЛЬНОСТИ ТЕКСТА.')
-#         # Поле ввода текста. value - значение по умолчанию.
-#         context = st.text_input('CONTEXT:', value='I love this beautiful world!')
-#         # Кнопка, нажатие на которую запускает процесс определения тональности.
-#         result = st.button('ОПРЕДЕЛИТЬ ТОНАЛЬНОСТЬ')
-#         if result:
-#             # Моя функция sentiment_and_score возвращает кортеж, в котором первый элемент
-#             # собственно тональность, а второй - оценка (score) =>
-#             # в следующей строке делаем распаковку кортежа, и вывод результатов.
-#             label, score = sentiment_and_score(context)
-#             st.text(f'LABEL={label}\nSCORE={score}')
-#     except ValueError:
-#         st.text('ЗАПОЛНИТЕ ПОЛЕ ВВОДА!')
-
 if __name__ == '__main__':
     try:
         # Определение тональности текста.
